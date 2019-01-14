95a75eb7fd0e2be7c2f894741824177c04a18e95
tomo taka <tomo_taka01@hotmail.com>
2019-01-14 18:04:32 +0900

    fix for showing Japanese era

---
+++ README.md
@@ -0,0 +1,5 @@
+## Sample using Spring Boot 2.1 with Java11 to show Japanese era
+
+### execute
+* gradle bootRun
+
--- src/main/java/com/example/japanesedatesample/controller/JapaneseDateController.java
+++ src/main/java/com/example/japanesedatesample/controller/JapaneseDateController.java
@@ -2,8 +2,16 @@ package com.example.japanesedatesample.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.chrono.JapaneseDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -14,8 +22,18 @@ public class JapaneseDateController {
     }
 
     @PostMapping("/show")
-    public String show(String inDate) {
+    public String show(String inDate, Model model) {
         log.info("show inDate=[{}]", inDate);
+        if (StringUtils.isEmpty(inDate)) {
+            model.addAttribute("japaneseDate", "???");
+        } else {
+            var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            var localDate= LocalDate.parse(inDate, formatter);
+            log.info("localDate=[{}]", localDate);
+            var japaneseDate = JapaneseDate.from(localDate);
+            model.addAttribute("japaneseDate", japaneseDate);
+        }
+        model.addAttribute("inDate", inDate);
         return "test";
     }
 }
