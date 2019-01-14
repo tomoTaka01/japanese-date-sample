a89fa15bf63fe555888fd935b82aafd685e60aee
tomo taka <tomo_taka01@hotmail.com>
2019-01-14 17:10:30 +0900

    first commit

---
+++ src/main/java/com/example/japanesedatesample/JapaneseDateSampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.japanesedatesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaneseDateSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapaneseDateSampleApplication.class, args);
+	}
+
+}
+
